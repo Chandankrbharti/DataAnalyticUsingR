75188fcea70cf4b3f26ca17ffc42b8bea22f467d
Chandankrbharti <bhartichandankr@gmail.com>
2018-06-20 16:01:39 +0530

    Add files via upload

--- Crime_sample.R
+++ Crime_sample.R
@@ -12,3 +12,21 @@ traniData_temp<-traindata[,-c(1,2,14,15,17,18)]
 colSums(is.na(traniData_temp))
 train_data_<-traniData_temp[complete.cases(traniData_temp),]
 View(train_data_)
+
+mytree<- tree(Primary.Type~ Latitude+Longitude,data=train_data_)
+mytree
+plot(mytree)
+text(mytree)
+str(train_data_)
+
+myforesat<-randomForest(Primary.Type~ Latitude+Longitude,data=train_data_,ntree=500)
+plot(myforesat)
+
+
+test_Data_url<-"C:/Users/Administrator/Documents/Test_Crime.csv"
+traindata_test<-read.csv(test_Data_url)
+View(traindata_test)
+
+Prediction<-predict(myforesat,newdata=traindata_test,type = "class")
+
+gviz<-leaflet(traindata_test)
